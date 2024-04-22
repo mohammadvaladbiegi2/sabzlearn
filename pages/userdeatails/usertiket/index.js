e756ed81be55338559762114e659726ce4af5de1
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2024-04-22 15:34:18 +0330

    complite ui tickets page

--- pages/userdeatails/usertiket/index.js
+++ pages/userdeatails/usertiket/index.js
@@ -2,12 +2,13 @@ import Sidebaruserdeatails from "@/components/Sidebaruserdeatails";
 import Userdeatailsheaderbox from "@/components/Userdeatailsheaderbox";
 import Image from "next/image";
 import React from "react";
-import { AiOutlineDollar } from "react-icons/ai";
 import { IoSunnyOutline } from "react-icons/io5";
 import { IoNotificationsOutline } from "react-icons/io5";
-import { SlRocket } from "react-icons/sl";
-import { FaCreditCard } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { BsTicketPerforated } from "react-icons/bs";
+import { IoMailOpenOutline } from "react-icons/io5";
+import { HiOutlineChatAlt2 } from "react-icons/hi";
+import { FiPlusCircle } from "react-icons/fi";
 
 export default function UserTiket() {
   return (
@@ -35,23 +36,75 @@ export default function UserTiket() {
         </header>
         <div className="flex flex-wrap gap-x-3 gap-y-4 md:gap-x-10 mb-10">
           <Userdeatailsheaderbox
-            title="دوره‌های ثبت نام شده"
-            desc="24 دوره"
-            icon={<FaCreditCard />}
+            title="همه تیکت ها"
+            desc="2 عدد"
+            icon={<BsTicketPerforated />}
             bg="bg-amber-400"
           />
           <Userdeatailsheaderbox
-            title="دوره‌های نقدی"
-            desc="11 دوره"
-            icon={<AiOutlineDollar />}
+            title="تیکت های باز"
+            desc="0 تیکت"
+            icon={<IoMailOpenOutline />}
             bg="bg-sky-500"
           />
           <Userdeatailsheaderbox
-            title="دوره‌های رایگان"
-            desc="13 دوره"
-            icon={<SlRocket />}
-            bg="bg-green-400"
+            title="تیکت های بسته شده"
+            desc="2 تیکت"
+            icon={<HiOutlineChatAlt2 />}
+            bg="bg-rose-500"
           />
+        </div>
+        <div className="mb-3">
+          <Userdeatailsheaderbox
+            desc="تیکت جدید"
+            icon={<FiPlusCircle />}
+            bg="bg-secondry"
+          />
+        </div>
+        <div className="bg_black_100 my-6  p-3.5 md:p-4.5 rounded-2xl">
+          <div class="flex justify-between items-center pb-3.5 md:pb-4.5 mb-6 md:mb-7 border-b border-b-gray-700">
+            <span class=" md:text-xl text-white">تیکت ها</span>
+          </div>
+          <div>
+            <div class="flex items-center justify-between flex-wrap gap-y-3 p-3 hover:bg-gray-700 rounded-xl transition-colors">
+              <div class="flex items-center">
+                <span class="block text-white w-20 text-right ">#10089</span>
+                <a href="#" class="text-white w-full  sm:max-w-md md:truncate">
+                  اضافه کردن دوره
+                </a>
+              </div>
+              <div class="flex items-center gap-5">
+                <span class="text-xs text-slate-400 text-ltr">
+                  1402/11/01 (19:17)
+                </span>
+                <span class="text-xs py-1 px-1.5 text-yellow-400  bg-yellow-400/10 rounded">
+                  ارتباط با مدیریت
+                </span>
+                <span class="text-xs py-1 px-1.5 text-yellow-400  bg-yellow-400/10 rounded">
+                  بسته شده
+                </span>
+              </div>
+            </div>
+            <div class="flex items-center justify-between flex-wrap gap-y-3 p-3 hover:bg-gray-700 rounded-xl transition-colors">
+              <div class="flex items-center">
+                <span class="block text-white w-20 text-right ">#3021</span>
+                <a href="#" class="text-white w-full  sm:max-w-md md:truncate">
+                  کانال پشتیبانی دوره ری اکت
+                </a>
+              </div>
+              <div class="flex items-center gap-5">
+                <span class="text-xs text-slate-400 text-ltr">
+                  1401/11/18 (23:48)
+                </span>
+                <span class="text-xs py-1 px-1.5 text-yellow-400  bg-yellow-400/10 rounded">
+                  پشتیبانی
+                </span>
+                <span class="text-xs py-1 px-1.5 text-yellow-400  bg-yellow-400/10 rounded">
+                  بسته شده
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
