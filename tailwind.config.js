bd22fd439b46a8c9f951f9172f58d910ff87ea34
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2024-04-29 21:33:55 +0330

    complite ui search page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        'COLOR1-HA': '#333C4C',
-        'COLOR2-HA': '#242A38',
-        'COLOR3-HA': '#0EA5E9',
-      }
     },
   },
   plugins: [],
-}
+};
