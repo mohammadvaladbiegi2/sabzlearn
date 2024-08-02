1085b978b301c363400d59ff8351b9daaf23c26a
mohammadvaladbiegi2 <mohammadvaladbiegi2@gmail.com>
2024-08-02 22:51:54 +0330

    fix

--- pages/api/auth/signout.js
+++ pages/api/auth/signout.js
@@ -5,13 +5,6 @@ export default function SignOutapi(req, res) {
     return res.status(422).json("know Method || Use Get Method");
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
 
   return res
     .setHeader(
